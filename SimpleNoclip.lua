30f991de99635e0fef837293c3da5c710fb30d40
Sergey <brawlstarshack9999@gmail.com>
2025-07-08 05:31:18 +0300

    Update SimpleNoclip.lua

--- SimpleNoclip.lua
+++ SimpleNoclip.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/SeRh1O4306/SimpleNoclip/refs/heads/main/RobloxNoclip.lua?token=GHSAT0AAAAAADG6YOAS2EBXOWMGRPDWRC4E2DMQA6A")()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SeRh1O4306/Simple-Noclip/refs/heads/main/RobloxNoclip.lua")()
